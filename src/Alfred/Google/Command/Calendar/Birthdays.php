5e199742ae3891e96fbaeec1ae647b4b641e3a7e
Jon Håvard Gundersen <jhg@xait.no>
2016-05-22 22:51:26 +0200

    Added command for listing out upcoming birthdays

---
+++ src/Alfred/Google/Command/Calendar/Birthdays.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Alfred\Google\Command\Calendar;
+
+use Alfred\Google\Calendar\PrintEvents;
+use Alfred\Google\Calendar\ListEvents;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Birthdays extends Command {
+
+	protected function configure() {
+		$this->setName('calendar:birthdays');
+		$this->setDescription('Show all birthdays happening next 30 days');
+		
+		parent::configure();
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$service_factory = $this->getServiceFactory($input);
+		$date = $this->getStartDate($input);
+
+		$searcher = new ListEvents($service_factory);
+		$searcher->setMinTime($date);
+		$searcher->setMaxTime($date->modify('+30 days'));
+		$searcher->setCalendars(['#contacts@group.v.calendar.google.com']);
+
+		$printer = new PrintEvents('D d.m H:i');
+		$printer->printEvents($searcher->search());
+	}
+}
